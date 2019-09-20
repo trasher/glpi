3076c01be785704b246f2f37a5cbf3600efe0821
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-09-20 11:19:57 +0200

    Missing backports

--- inc/dbmysql.class.php
+++ inc/dbmysql.class.php
@@ -1754,4 +1754,22 @@ class DBmysql {
       }
       return trim($this->removeSqlComments($output));
    }
+
+   public static function getQuoteNameChar(): string {
+      return '`';
+   }
+
+   /**
+    * Is value quoted as database field/expression?
+    *
+    * @param string|\QueryExpression $value Value to check
+    *
+    * @return boolean
+    *
+    * @since 10.0.0
+    */
+   public static function isNameQuoted($value): bool {
+      $quote = static::getQuoteNameChar();
+      return is_string($value) && trim($value, $quote) != $value;
+   }
 }
