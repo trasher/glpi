19ba864088020606663fad863ef164a1a2bfdc9e
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-09-20 09:20:51 +0200

    Use Search to display sub lists
    Sublists are used for:
    - volumes
    - virtual machines
    - remote management
    - contracts items
    - documents
    - certificates

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -2334,6 +2334,7 @@ class Dropdown {
 
       $table = $item->getTable();
       $datas = [];
+      $search_inst = new Search($item, []);
 
       $displaywith = false;
       if (isset($post['displaywith'])) {
@@ -2416,7 +2417,7 @@ class Dropdown {
             $where["$table.id"] = $one_item;
          } else {
             if (!empty($post['searchText'])) {
-               $search = Search::makeTextSearchValue($post['searchText']);
+               $search = $search_inst->makeTextSearchValue($post['searchText']);
 
                $swhere = [
                   "$table.completename" => ['LIKE', $search],
@@ -2789,7 +2790,7 @@ class Dropdown {
          }
 
          if (!empty($post['searchText'])) {
-            $search = Search::makeTextSearchValue($post['searchText']);
+            $search = $search_inst->makeTextSearchValue($post['searchText']);
             $orwhere = ["$table.$field" => ['LIKE', $search]];
 
             if ($_SESSION['glpiis_ids_visible']
@@ -3128,7 +3129,8 @@ class Dropdown {
       }
 
       if (isset($post['searchText']) && (strlen($post['searchText']) > 0)) {
-         $search = Search::makeTextSearchValue($post['searchText']);
+         $search_inst = new Search($item, []);
+         $search = $search_inst->makeTextSearchValue($post['searchText']);
          $where['OR'] = [
             "$table.name"        => ['LIKE', $search],
             "$table.otherserial" => ['LIKE', $search],
@@ -3321,7 +3323,8 @@ class Dropdown {
       }
 
       if (isset($_POST['searchText']) && (strlen($post['searchText']) > 0)) {
-         $search = ['LIKE', Search::makeTextSearchValue($post['searchText'])];
+         $search_inst = new Search($item, []);
+         $search = ['LIKE', $search_inst->makeTextSearchValue($post['searchText'])];
          $orwhere =[
             'name'   => $search,
             'id'     => $post['searchText']
@@ -3485,9 +3488,11 @@ class Dropdown {
       }
 
       if (isset($post['searchText']) && strlen($post['searchText']) > 0) {
+         $search_inst = new Search(new Netpoint(), []);
+         $search = ['LIKE', $search_inst->makeTextSearchValue($post['searchText'])];
          $criteria['WHERE']['OR'] = [
-            'glpi_netpoints.name'         => ['LIKE', Search::makeTextSearchValue($post['searchText'])],
-            'glpi_locations.completename' => ['LIKE', Search::makeTextSearchValue($post['searchText'])]
+            'glpi_netpoints.name'         => $search,
+            'glpi_locations.completename' => $search
          ];
       }
 
