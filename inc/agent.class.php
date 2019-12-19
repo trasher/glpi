33b3c977494c79a1b8f60fe8d98ae29d6e8e7f38
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-12-19 11:20:35 +0100

    Fix agent type

--- inc/agent.class.php
+++ inc/agent.class.php
@@ -288,14 +288,14 @@ class Agent extends CommonDBTM {
       }
 
       $atype = new AgentType();
-      $atypes_id = $atype->getFromDBByCrit(['name' => 'Core']);
+      $atype->getFromDBByCrit(['name' => 'Core']);
 
       $input = [
          'deviceid'     => $deviceid,
          'name'         => $deviceid,
          'last_contact' => date('Y-m-d H:i:s'),
          'useragent'    => $_SERVER['HTTP_USER_AGENT'] ?? null,
-         'agenttypes_id'=> $atypes_id,
+         'agenttypes_id'=> $atype->fields['id'],
          'itemtype'     => $metadata['itemtype'] ?? 'Computer'
       ];
 
@@ -339,5 +339,134 @@ class Agent extends CommonDBTM {
 
    public function prepareInputForUpdate($input) {
       return $this->prepareInputs($input);
+   }
+
+   /**
+    * Display agent information for a computer
+    *
+    * @param Computer the computer
+    * @param integer $colspan the number of columns of the form (2 by default)
+    */
+   public function showInventoryInfos(CommonDBTM $item, $colspan = 2) {
+      $res = $this->getFromDBByCrit(['itemtype' => $item->getType(), 'items_id' => $item->fields['id']]);
+
+      if (!$res) {
+         return;
+      }
+
+      echo '<tr class="tab_bg_1">';
+      echo '<td>'.__('Agent').'</td>';
+      echo '<td>'.$this->getLink(1).'</td>';
+
+      if ($colspan == 2) {
+         echo '</tr>';
+         echo '<tr class="tab_bg_1">';
+      }
+
+      echo '<td>'.__('Useragent').'</td>';
+      echo '<td>'.$this->fields['useragent'].'</td>';
+      echo '</tr>';
+
+      if (isset($this->fields['id']) && !is_null($item) && isset($item->fields['id'])) {
+         $agent_id = $this->fields['id'];
+         $fi_path = $CFG_GLPI['root_doc']."/plugins/fusioninventory";
+
+         echo "<tr class='tab_bg_1'>";
+         echo "<td>";
+         echo __('Status')."&nbsp;:";
+         echo "</td>";
+         echo "<td>";
+
+         $load_anim   = '<i class="fas fa-sync fa-spin fa-fw"></i>';
+
+         echo Html::scriptBlock("$(function() {
+            var waiting = false;
+
+            var refresh_status = function(display_refresh) {
+               var display_refresh = (typeof display_refresh !== 'undefined') ? display_refresh : true;
+               $('#agent_status').html('$load_anim');
+               $('#refresh_status').hide();
+               $('#force_inventory_button').hide();
+
+               $.get('$fi_path/ajax/remote_status.php', {
+                  id: $agent_id,
+                  action: 'get_status'
+               }, function(answer) {
+                  if (typeof answer.waiting != 'undefined'
+                     && answer.waiting == true) {
+                     $('#force_inventory_button').show();
+                     waiting = true;
+                  }
+
+                  $('#agent_status').html(answer.message);
+                  if (display_refresh) {
+                     $('#refresh_status').show();
+                  }
+               });
+            };
+
+            var force_inventory = function() {
+               $('#agent_status').html('$load_anim');
+               $('#refresh_status').hide();
+               waiting = false;
+               $('#force_inventory_button').hide();
+               $.get('$fi_path/ajax/remote_status.php', {
+                  id: $agent_id,
+                  action: 'start_agent'
+               }, function(answer) {
+                  refresh_status(false);
+                  displayAjaxMessageAfterRedirect();
+
+                  // add a loop for checking status (set a max iterations to avoid infinite looping)
+                  var loop_index = 0;
+                  var myloop = setInterval(function() {
+                     if (loop_index > 30 || waiting) {
+                        clearInterval(myloop);
+                        $('#refresh_status').show();
+                        return;
+                     }
+                     refresh_status(false);
+                     loop_index++;
+                  }, 2000);
+               });
+            };
+
+            $(document)
+               .on('click', '#refresh_status', function() {
+                  refresh_status();
+               })
+               .on('click', '#force_inventory_button', function() {
+                  force_inventory();
+               });
+         });");
+         echo "<span id='agent_status'>".
+            __("not yet requested, refresh?", 'fusioninventory').
+            "</span>";
+         echo "<span id='refresh_status'><i class='fas fa-sync'></i></span>";
+         echo "</td>";
+
+         echo "<td colspan='2'>";
+         echo "<span id='force_inventory_button'>".
+            __('Force inventory', 'fusioninventory').
+            "</span>";
+         echo "</td>";
+         echo "</tr>";
+      }
+
+      echo '<tr class="tab_bg_1">';
+      echo '<td>'.__('Inventory tag').'</td>';
+      echo '<td>'.$this->fields['tag'].'</td>';
+      if ($colspan == 2) {
+         echo '</tr>';
+         echo '<tr class="tab_bg_1">';
+      }
+
+      echo '<td>';
+      echo __('Last contact');
+      echo '</td>';
+      echo '<td>';
+      echo Html::convDateTime($this->fields['last_contact']);
+      echo '</td>';
+      echo '</tr>';
    }
 }
